4b3f92405447f42557dc772f857bf296c72674f1
Georden-Hudson-Gessford <154949008+Georden-Hudson-Gessford@users.noreply.github.com>
2026-08-10 19:39:50 -0700

    Add Compiler_Parser.h for parsing functionality

---
+++ 0.1/Include/Compiler_Parser.h
@@ -0,0 +1,78 @@
+#ifndef PRETTY_PARSER
+#define PRETTY_PARSER
+
+#include <iostream> 
+#include <string> 
+#include <vector>
+#include <memory>
+#include <stdexcept>
+#include "Token_Maker.h"
+
+
+enum class NodeType {
+
+add,
+minus,
+multiply,
+divide,
+unary,
+
+number, 
+identifier,
+
+less,
+greater,
+greq,
+leq,
+is,
+Not,
+
+if_stmt,
+var_assign,
+print_stmt,
+block,
+
+condition 
+
+
+};
+
+struct Node {
+std::string string_token;
+std::vector<std::unique_ptr<Node>> block;
+NodeType type;
+std::unique_ptr<Node> left = nullptr;
+std::unique_ptr<Node> right = nullptr;
+
+};
+
+
+
+class parser {
+private:
+std::vector<token> tokens;
+size_t current = 0;
+
+
+public:
+    explicit parser(std::vector<token> input_tokens)
+        : tokens(input_tokens){}
+
+    const token& peek();
+    const token& consume(TokenType expected);
+    std::unique_ptr<Node> parse_number();
+    std::unique_ptr<Node> parse_expression();
+    std::unique_ptr<Node> parse_term();
+    std::unique_ptr<Node> parse_factor();
+    std::unique_ptr<Node> parse_primary();
+    std::unique_ptr<Node> parse_identifier();
+    std::unique_ptr<Node> parse_assignment();
+    std::unique_ptr<Node> parse_condition();
+    std::unique_ptr<Node> parse_write();
+    std::unique_ptr<Node> parse_if();
+    std::unique_ptr<Node> parse_statement();
+    std::vector<std::unique_ptr<Node>> parse_program();
+};
+
+
+#endif
